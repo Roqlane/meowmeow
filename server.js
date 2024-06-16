c922c53b98575fe53e6f0e67ef7c83ccbd604e2f
Roqlane <78229498+Roqlane@users.noreply.github.com>
2024-06-16 21:23:50 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,29 @@
+if (process.env.NODE_ENV !== 'production') {
+    require("dotenv").config()
+}
+
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts")
+const bodyParser = require("body-parser")
+const methodOverride = require("method-override")
+
+app = express();
+
+app.use(express.json())
+app.use(express.static("public"))
+app.set("view engine", "ejs")
+app.set("layout", "layouts/layout")
+app.use(expressLayouts)
+app.use(methodOverride('_method'))
+app.use(bodyParser.urlencoded({limit: "10mb", extended: true}))
+// app.use(express.static(__dirname + 's/public/img')); 
+ 
+//test sur la page principal
+app.get("/", (req, res) => {
+    res.render("<h1>meow</h1>)
+})
+
+
+app.listen(3000, () => {
+    console.log("Listening on  port 3000")
+})
